907c3f9b01a831175dd7cf59ef703f465d317f93
Matt Boynes <mboynes@alleyinteractive.com>
2013-08-05 19:25:40 -0400

    Description update

--- debug-bar-remote-requests.php
+++ debug-bar-remote-requests.php
@@ -2,29 +2,25 @@
 /*
 	Plugin Name: Debug Bar Remote Requests
 	Plugin URI: http://www.alleyinteractive.com/
-	Description: A simple add-on for Debug Bar that logs and profiles all remote requests
+	Description: A simple add-on for Debug Bar that logs and profiles all remote requests made using the HTTP Request API
 	Version: 0.1
 	Author: Matthew Boynes
 	Author URI: http://www.alleyinteractive.com/
 */
 /*  This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
-
-/**
- *
- */
 
 if ( !class_exists( 'Debug_Bar_Remote_Requests' ) ) :
 
